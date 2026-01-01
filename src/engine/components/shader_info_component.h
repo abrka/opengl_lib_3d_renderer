7f49c5e47f27733995810356bd8c94b0a538c816
abrka <ifradnashit@gmail.com>
2026-01-01 08:02:40 +0600

    add default shader when creating shader info component

--- src/engine/components/shader_info_component.h
+++ src/engine/components/shader_info_component.h
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <string>
 #include <filesystem>
 
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
 namespace Engine {
+	const std::string default_vertex_shader = TOSTRING(ENGINE_ASSET_DIR) + std::string{ "/shaders/pbr_vertex.glsl" };
+	const std::string default_fragment_shader = TOSTRING(ENGINE_ASSET_DIR) + std::string{ "/shaders/pbr_frag.glsl" };
+	
 	struct ShaderInfoComponent {
-		std::filesystem::path vertex_filepath{};
-		std::filesystem::path fragment_filepath{};
+		std::filesystem::path vertex_filepath{default_vertex_shader};
+		std::filesystem::path fragment_filepath{default_fragment_shader};
 	};
 }
